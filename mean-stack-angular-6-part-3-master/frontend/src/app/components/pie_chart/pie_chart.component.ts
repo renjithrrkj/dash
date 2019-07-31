a4fbdaa755ccb8c06176ae1540294c44e38af93b
renjithrrkj <renjithrrkj@gmail.com>
2019-07-31 20:42:06 +0530

    pie chart online

--- mean-stack-angular-6-part-3-master/frontend/src/app/components/pie_chart/pie_chart.component.ts
+++ mean-stack-angular-6-part-3-master/frontend/src/app/components/pie_chart/pie_chart.component.ts
@@ -21,22 +21,23 @@ import * as pluginDataLabels from 'chartjs-plugin-datalabels';
     public pieChartOptions: ChartOptions = {
         responsive: true,
         legend: {
-          position: 'top',
+          position: 'left',
         },
-        plugins: {
+        /*plugins: {
           datalabels: {
             formatter: (value, ctx) => {
               const label = ctx.chart.data.labels[ctx.dataIndex];
               return label;
             },
           },
-        }
+          
+        }*/
       };
       public pieChartLabels: Label[] = [['Download', 'Sales'], ['In', 'Store', 'Sales'], 'Mail Sales'];
       public pieChartData: number[] = [300, 500, 100];
       public pieChartType: ChartType = 'pie';
       public pieChartLegend = true;
-      public pieChartPlugins = [pluginDataLabels];
+      //public pieChartPlugins = [pluginDataLabels];
       public pieChartColors = [
         {
           backgroundColor: ['rgba(255,0,0,0.3)', 'rgba(0,255,0,0.3)', 'rgba(0,0,255,0.3)'],
