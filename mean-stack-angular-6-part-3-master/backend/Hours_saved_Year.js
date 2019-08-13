e2d8aad16f6684b7801c82bf9a0cca9771b25011
Renjith <renjithrrkj@gmail.com>
2019-08-13 08:21:23 +0530

    changed ip of json serve

--- mean-stack-angular-6-part-3-master/backend/Hours_saved_Year.js
+++ mean-stack-angular-6-part-3-master/backend/Hours_saved_Year.js
@@ -2,7 +2,7 @@ var request = require('request');
  
 module.exports={get_hours_year:function(res){
 var jira=[];var Team_date =[];var count_of_Tests=0;
-request("http://localhost:3000/test",{json:true},(err,res,body)=>{
+request("http://172.31.26.196:89/test",{json:true},(err,res,body)=>{
     
     if (err) { return console.log(err); }
     console.log(body.url);
